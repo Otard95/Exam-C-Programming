050b350cb26cb141f8691501725ef8608facc5f7
otard95 <stian.myklebostad@getmail.no>
2017-11-12 14:06:15 +0100

    moved all tests to tests.c

--- main.c
+++ main.c
@@ -1,17 +1,6 @@
 #include "inc/btree.h"
+#include "inc/tests.h"
 #include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-
-void print_sub_nodes(char *path, NODE_TYPE type, StringInt val) {
-
-  if (type == INTEGER_NODE) {
-    printf("%s = %d\n", path, val.i);
-  } else {
-    printf("%s = \"%s\"\n", path, val.c);
-  }
-
-}
 
 int main(int argc, char *argv[]) {
 
@@ -23,14 +12,6 @@ int main(int argc, char *argv[]) {
     return -1;
   }
 
-  /* Variables for temporarily holding:
-   * path, name, value, type, and status-code
-   * for each test.
-   */
-  char name[20];
-  char path[80];
-  StringInt val;
-  NODE_TYPE type;
   STATUS_CODE sc;
 
   /* ### Setup ### */
@@ -49,102 +30,8 @@ int main(int argc, char *argv[]) {
   printf("B-tree setup done.\n");
   printf("\n----------------------------------\n\n");
 
-  // ### GetStr() Test
-  printf("GetStr() Test:\n");
-
-  strcpy(path, "strings.no.text");
-  val.c = GetStr(root, path, &sc);
-  if (sc != OK) {
-    printf("Failed to get 'strings.no.text'\n");
-  } else {
-    printf("strings.no.text = %s\n", val.c);
-  }
-  printf("\n----------------------------------\n\n");
-
-  // ### GetInt() Test
-  printf("GetInt() Test:\n");
-
-  strcpy(path, "config.update.interval");
-  val.i = GetInt(root, path, &sc);
-  if (sc != OK) {
-    printf("Failed to get 'config.update.interval'\n");
-  } else {
-    printf("config.update.interval = %d\n", val.i);
-  }
-  printf("\n----------------------------------\n\n");
-
-  // ### Testing GetType() and GetValue()
-  printf("Testing GetType() in conjunction with GetValue():\n");
-
-  strcpy(path, "config.update.server1");
-  type = GetType(root, path, &sc);
-  if (sc != OK) {
-    printf("Could not get type of 'config.update.server1'\n");
-  } else {
-    printf("Type of 'config.update.server1' was '%s'\n", type == STRING_NODE ? "String" : "Int");
-
-    // So lets do the GetValue part
-    strcpy(path, "config.update.server1");
-
-    val = GetValue(root, path, &sc);
-    if (sc != OK) {
-      printf("Could not get value of 'config.update.server1'\n");
-    } else {
-      if (type == STRING_NODE) {
-        printf("'config.update.server1' = %s\n", val.c);
-      } else {
-        printf("'config.update.server1' = %d\n", val.i);
-      } // END if type chech
-    } // END if status check
-  } // END if status check of GetType()
-
-  printf("\n");
-
-  // Now on a integer node
-  strcpy(path, "config.update.timeout");
-  type = GetType(root, path, &sc);
-  if (sc != OK) {
-    printf("Could not get type of 'config.update.timeout'\n");
-  } else {
-    printf("Type of 'config.update.timeout' was '%s'\n", type == STRING_NODE ? "String" : "Int");
-
-    // So lets do the GetValue part
-    strcpy(path, "config.update.timeout");
-
-    val = GetValue(root, path, &sc);
-    if (sc != OK) {
-      printf("Could not get value of 'config.update.timeout'\n");
-    } else {
-      if (type == STRING_NODE) {
-        printf("'config.update.timeout' = %s\n", val.c);
-      } else {
-        printf("'config.update.timeout' = %d\n", val.i);
-      } // END if type chech
-    } // END if status check
-  } // END if status check of GetType()
-  printf("\n----------------------------------\n\n");
-
-  // ### END GetType() and GetValue() Test
-
-  // Enumerate() Test
-  printf("Enumerate() Test:\n");
-
-  strcpy(path, "strings.*.*");
-  printf("Path to Enumerate = '%s'\n\n", path);
-  Enumerate(root, path, &print_sub_nodes);
-  printf("\n----------------------------------\n\n");
-
-  // ### Delete() Test
-  printf("Delete() Test:\n");
-
-  strcpy(path, "strings.no.header");
-  sc = Delete(root, path);
-  if (sc != OK) {
-    printf("Failed to delete\n");
-  } else {
-    printf("Deleted 'strings.no.header'\n");
-  }
-  printf("\n----------------------------------\n\n");
+  // Run Test's
+  tests(root);
 
   // ### Cleanup
   printf("Cleanup...\n");
